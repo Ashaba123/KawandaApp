716851493803c4b28790166fdc31e95b0aa31e20
Ashaba Derrick <derrick@victorium.biz>
2020-08-08 19:00:29 +0300

    changes on the sub process page

--- Kawanda/app/src/main/java/com/learn/kawandafood/ui/process/SubProcessAdapter.java
+++ Kawanda/app/src/main/java/com/learn/kawandafood/ui/process/SubProcessAdapter.java
@@ -9,10 +9,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.learn.kawandafood.R;
+import com.learn.kawandafood.data.entity.Client;
 import com.learn.kawandafood.data.entity.SubProcess;
+import com.learn.kawandafood.data.viewmodel.ClientViewModel;
+import com.learn.kawandafood.data.viewmodel.SubProcessViewModel;
 
 import java.util.List;
 
@@ -54,6 +59,18 @@ public class SubProcessAdapter extends RecyclerView.Adapter<SubProcessAdapter.Vi
             super(itemView);
             processName = itemView.findViewById(R.id.sub_process_name);
             imgDelete = itemView.findViewById(R.id.img_delete_sub_process);
+
+            imgDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    SubProcess subProcess = subProcesses.get(position);
+                    SubProcessViewModel subProcessViewModel =
+                            new ViewModelProvider((ViewModelStoreOwner) v.getContext()).get(SubProcessViewModel.class);
+                    subProcessViewModel.deleteSubProcess(subProcess);
+                    Toast.makeText(v.getContext(), "Deleted", Toast.LENGTH_SHORT).show();
+                }
+            });
 
         }
 
